21f584acdb030b802e811ba648d3d569bbeb3543
Yuri Matos Rodrigues <yurimatos.tuti@gmail.com>
2022-08-14 23:19:47 -0300

    GET com FromQuery e FromRoute

--- RestAPI/RestAPI/Controllers/CidadesController.cs
+++ RestAPI/RestAPI/Controllers/CidadesController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using RestAPI.Models;
 using RestAPI.Repository;
+using System.ComponentModel.DataAnnotations;
 
 namespace RestAPI.Controllers
 {
@@ -9,13 +10,21 @@ namespace RestAPI.Controllers
     public class CidadesController : Controller
     {
        [HttpGet("paises/{idPais}/estados/{idEstado}/cidades")]
-       public List<Cidade> GetCidades([FromQuery] string nome)
+       public List<Cidade> GetCidades(
+           [FromQuery] string? nome,
+           [FromQuery, Range(1, 1000000)] int fromPopulacao,
+           [FromRoute, Required] int idPais,
+           [FromRoute, Required] int idEstado
+           )
         {
-            var resultado = CidadeRepository.Cidades;
+            var resultado = CidadeRepository.Cidades.Where(cidade => cidade.IdPais == idPais && 
+                cidade.IdEstado == idEstado).ToList();
+
             if (!string.IsNullOrEmpty(nome))
-            {
-                resultado = CidadeRepository.Cidades.Where(cidade => cidade.Nome == nome).ToList();
-            }
+                resultado = resultado.Where(cidade => cidade.Nome == nome).ToList();
+
+            if (fromPopulacao > 0)
+                resultado = resultado.Where(cidade => cidade.Populacao >= fromPopulacao).ToList();
             return resultado;
         }
     }
