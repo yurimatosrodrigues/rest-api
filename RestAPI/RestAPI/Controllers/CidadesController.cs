3f092475f0c0198764c69176e74adacb2bbaa2fc
Yuri Matos Rodrigues <yurimatos.tuti@gmail.com>
2022-08-15 23:34:41 -0300

    Criação do método POST

--- RestAPI/RestAPI/Controllers/CidadesController.cs
+++ RestAPI/RestAPI/Controllers/CidadesController.cs
@@ -5,12 +5,12 @@ using System.ComponentModel.DataAnnotations;
 
 namespace RestAPI.Controllers
 {
-    [Route("")]
+    [Route("paises/{idPais}/estados/{idEstado}/cidades")]
     [ApiController]
     public class CidadesController : Controller
     {
-       [HttpGet("paises/{idPais}/estados/{idEstado}/cidades")]
-       public List<Cidade> GetCidades(
+       [HttpGet]
+       public ActionResult<List<Cidade>> GetCidades(
            [FromQuery] string? nome,
            [FromQuery, Range(1, 1000000)] int fromPopulacao,
            [FromRoute, Required] int idPais,
@@ -20,12 +20,30 @@ namespace RestAPI.Controllers
             var resultado = CidadeRepository.Cidades.Where(cidade => cidade.IdPais == idPais && 
                 cidade.IdEstado == idEstado).ToList();
 
+            if (resultado.Count == 0) return NotFound(null);
+
             if (!string.IsNullOrEmpty(nome))
                 resultado = resultado.Where(cidade => cidade.Nome == nome).ToList();
 
             if (fromPopulacao > 0)
                 resultado = resultado.Where(cidade => cidade.Populacao >= fromPopulacao).ToList();
-            return resultado;
+            return Ok(resultado);
+        }
+
+        [HttpPost]
+        public ActionResult PostCidades(
+            [FromRoute, Required] int idPais,
+            [FromRoute, Required] int idEstado,
+            [FromBody] Cidade cidade
+            )
+        {
+            cidade.IdEstado = idEstado;
+            cidade.IdPais = idPais;
+            CidadeRepository.Cidades.Add(cidade);
+            CidadeRepository.Save();
+
+            string locationUrl = $"paises/{idPais}/estados/{idEstado}/cidades/{cidade.Id}";
+            return Created(locationUrl, null);
         }
     }
 }
